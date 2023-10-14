ee8a436ec849681b2ff6f471f1ea18deaff739fc
hamle <hamle@DESKTOP-LCJ4CGJ>
2023-10-14 12:50:12 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Realizar un programa que lea cuatro valores numéricos e
+// informar su suma y promedioRealizar un programa que lea cuatro valores numéricos
+// e informar su suma y promedio
+
+using ConsoleApp3.ejercicio_3;
+
+Suma c;
+Console.WriteLine(" ingrese el pirmer valor numerico ");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" ingrese el segundo valor numerico ");
+double num2 = double.Parse(Console.ReadLine());
+Console.WriteLine(" ingrese el tercer valor numerico ");
+double num3 = double.Parse(Console.ReadLine());
+Console.WriteLine(" ingrese el cuarto valor numerico ");
+double num4 = double.Parse(Console.ReadLine());
+
+c = new Suma(num1, num2, num3, num4);
+
+Console.WriteLine(" la suma de los valores numericos es " +c.Getsuma());
+Console.WriteLine(" El promedio de los valores numericos es " +c.Promedio());
---
+++ ConsoleApp3/ejercicio 3/Suma.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.ejercicio_3
+{
+    internal class Suma
+    {
+        private double num1;
+        private double num2;
+        private double num3;
+        private double num4;
+
+        public Suma ( double num1, double num2, double num3, double num4)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+            this.num4 = num4;
+
+        }
+
+        public double Getsuma()
+        {
+            return num1 +num2 + num3 + num4;
+        }
+        public double Promedio()
+        {
+            return Getsuma() / 4;
+        }
+    }
+}
